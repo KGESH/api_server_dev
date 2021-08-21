9b69465824146105010226a78be726e2a18e08eb
youseonghyeon <dolla_@naver.com>
2021-08-21 21:46:41 +0900

    스키마, Model 새로 다시 짯습니다.

--- src/db/user/testQRModel.ts
+++ src/db/user/testQRModel.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema } from 'mongoose';
 
 export interface Iqr {
-  cafeName: string;
+  cafe_name: string;
   code: string;
+  visit_times: number;
 }
 
 const qrSchema = new Schema<Iqr>({
-  cafeName: String,
+  cafe_name: String,
   code: String,
+  visit_times: Number,
 });
 
 export const qrModel = mongoose.model('qr_test_version', qrSchema, 'test-qr');
