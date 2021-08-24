07c67bf7d7c470017dffe452c32b3b16703c31a8
youseonghyeon <dolla_@naver.com>
2021-08-24 17:02:16 +0900

    123

--- src/db/user/UserModel.ts
+++ src/db/user/UserModel.ts
@@ -1,31 +1,31 @@
 import mongoose, { Schema } from 'mongoose';
 
-export interface QR_data {
+export interface IQR {
   cafe_name: string;
   code: string;
   card_img: string;
   visit_times: number;
 }
 
-export interface User {
+export interface IUser {
   _id: string;
   id: number;
   name: string;
   email: string;
   auth: string;
-  cafe_list: [QR_data];
+  cafe_list: [IQR];
   point: number;
   average_star: number;
 }
 
-const QR_data = new Schema<QR_data>({
+const QR_data = new Schema<IQR>({
   cafe_name: String,
   code: String,
   card_img: String,
   visit_times: Number,
 });
 
-const userSchema = new Schema<User>({
+const userSchema = new Schema<IUser>({
   _id: String,
   id: Number,
   name: String,
