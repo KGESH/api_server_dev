ca00a3bd27d5181c5191c2a0528c389f14fb731e
junni11 <wkszlf98@naver.com>
2021-09-13 23:32:08 +0900

    commit

--- src/db/user/FindUser.ts
+++ src/db/user/FindUser.ts
@@ -3,7 +3,7 @@ import { UserModel } from '@db/user/UserModel';
 /* 현재 사용중 */
 export const FindAllUser = async () => await UserModel.find({});
 
-export const FindUserById = async (id: any) => await UserModel.findOne({ ...id });
+export const FindUserById = async (id: any) => await UserModel.findOne({ id });
 
 export const ExistCafeNameInUser = async (id: number, cafe_name: string) =>
   await UserModel.findOne({ id }).where('cafe_list').equals({ $elemMatch: { cafe_name } });
