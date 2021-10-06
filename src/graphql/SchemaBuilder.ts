5afcf31871a28c4a1735816cae1f599bf9a74127
KGESH <baram789456@gmail.com>
2021-10-06 17:30:19 +0900

    스키마 리졸버 모듈화. graphql 디렉토리 내부 레거시 디렉토리들에서 각자 작성한 코드들 모듈화 부탁드립니다.

--- src/graphql/SchemaBuilder.ts
+++ src/graphql/SchemaBuilder.ts
@@ -1,17 +1,13 @@
 import { loadSchemaSync } from '@graphql-tools/load';
-import { loadFiles, loadFilesSync } from '@graphql-tools/load-files';
-import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers } from '@graphql-tools/merge';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { addResolversToSchema } from '@graphql-tools/schema';
 import { join } from 'path';
 
-/** schema, resolver 병합 (21-09-04:지성현)*/
 const schema = loadSchemaSync(join(__dirname, './schema/**/*.graphql'), {
   loaders: [new GraphQLFileLoader()],
 });
-
-//const schemaArray = loadFilesSync(join(__dirname, './schema/**/*.graphql'));
-//export const schema = mergeTypeDefs(schemaArray);
 
 const resolverArray = loadFilesSync(join(__dirname, './resolver/**/*.resolvers.*'));
 export const resolvers = mergeResolvers(resolverArray);
